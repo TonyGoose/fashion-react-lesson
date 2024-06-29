7776d69a69dcf8d18dbf0123324e0ff6a6c8b239
TonyGoose <preparefight@yandex.ru>
2024-06-30 00:23:08 +0300

    cards/brands

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import Brands from "./components/brands/Brands";
 import Header from "./components/header/Header";
 import Promo from "./components/promo/Promo";
+import Arrivals from "./components/arrivals/Arrivals";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Promo />
+      <Brands />
+      <Arrivals />
     </div>
   );
 }
